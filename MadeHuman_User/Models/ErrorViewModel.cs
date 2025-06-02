aa20081c98c49b1175d79fe0e94f1b407de3f6be
mtie-stu <mtie290305@Qgmail.com>
2025-06-02 22:58:27 +0700

    Add_Project_User_Admin_Add_Share

---
+++ MadeHuman_User/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MadeHuman_User.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
