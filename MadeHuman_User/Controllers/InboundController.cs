3193eb55b7e69e845fe2f318d5bee270e3500901
ThanhCong1610 <congpltps38278@gmail.com>
2025-06-10 23:38:14 +0700

    Feture_FE_Layout_User

---
+++ MadeHuman_User/Controllers/InboundController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MadeHuman_User.Controllers
+{
+    public class InboundController : Controller
+    {
+        public IActionResult Import()
+        {
+            return View();
+        }
+        public IActionResult Export()
+        {
+            return View();
+        }
+    }
+}
