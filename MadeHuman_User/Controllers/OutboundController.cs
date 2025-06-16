2e40fddfa3095ee60518b4911b46492e336777ec
DinhGiATuong <tuongdgps38065@gmail.com>
2025-06-16 15:19:04 +0700

    Feature_Picker_B

---
+++ MadeHuman_User/Controllers/OutboundController.cs
@@ -0,0 +1,40 @@
+﻿using MadeHuman_User.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadeHuman_User.Controllers
+{
+    public class OutboundController : Controller
+    {
+        public IActionResult Picker()
+        {
+            var model = new PickerViewModel
+            {
+                TaskId = 3122,
+                ProductItem = "SP001",
+                LocationStorage = "A1-02",
+                Baskets = "5",
+                Quantity = 20,
+                ProductName = "Sản phẩm mẫu",
+                ImageUrl = "/images/sample-product.png" // Đảm bảo ảnh tồn tại trong wwwroot/images/
+            };
+            return View(model);
+        }
+
+        public IActionResult CheckerTaskMix()
+        {
+            return View();
+        }
+        public IActionResult CheckerTaskSingle()
+        {
+            return View();
+        }
+        public IActionResult Packer()
+        {
+            return View();
+        }
+        public IActionResult Dispatch()
+        {
+            return View();
+        }
+    }
+}
---
+++ MadeHuman_User/Models/CheckerTaskMixViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace MadeHuman_User.Models
+{
+    public class CheckerTaskMixViewModel
+    {
+        public string TaskId { get; set; }
+        public string ProductItem { get; set; }
+        public string LocationStorage { get; set; }
+        public string Baskets { get; set; }
+        public int Quantity { get; set; }
+        public string ImageUrl { get; set; }
+        public List<CheckerTaskItem> ProductTasks { get; set; }
+    }
+
+    public class CheckerTaskItem
+    {
+        public string Status { get; set; }
+        public string SmallTaskID { get; set; }
+        public string ProductItemID { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public string LocationStorage { get; set; }
+    }
+}
