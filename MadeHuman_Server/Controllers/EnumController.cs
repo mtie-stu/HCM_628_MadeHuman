6e9f25672b811ca7b2fbc4739e530a5144e43bb3
mtie-stu <mtie290305@Qgmail.com>
2025-06-26 15:36:39 +0700

    Deloy_Feature_BE_Sheet

---
+++ MadeHuman_Server/Controllers/EnumController.cs
@@ -0,0 +1,55 @@
+﻿using MadeHuman_Server.Model.User_Task;
+using Madehuman_Share.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadeHuman_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnumController : ControllerBase
+    {
+       
+        [HttpGet("status-parttime")]
+            public IActionResult GetStatusPartTime()
+            {
+                var result = Enum.GetValues(typeof(StatusPartTime))
+                    .Cast<StatusPartTime>()
+                    .Select(e => new EnumOptionViewModel
+                    {
+                        Value = (int)e,
+                        Name = e.ToString()
+                    }).ToList();
+
+                return Ok(result);
+            }
+
+            [HttpGet("status-company")]
+            public IActionResult GetStatusCompany()
+            {
+                var result = Enum.GetValues(typeof(StatusPart_Time_Company))
+                    .Cast<StatusPart_Time_Company>()
+                    .Select(e => new EnumOptionViewModel
+                    {
+                        Value = (int)e,
+                        Name = e.ToString()
+                    }).ToList();
+
+                return Ok(result);
+            }
+
+            [HttpGet("task-type")]
+            public IActionResult GetTaskType()
+            {
+                var result = Enum.GetValues(typeof(TaskType))
+                    .Cast<TaskType>()
+                    .Select(e => new EnumOptionViewModel
+                    {
+                        Value = (int)e,
+                        Name = e.ToString()
+                    }).ToList();
+
+                return Ok(result);
+            }
+    }
+}
